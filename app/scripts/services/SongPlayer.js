3cf1774706c21a3428696ae09630827286aef11d
Doug Major <demajor@demajorMBP.local>
2017-01-03 14:32:33 -0800

    assignment 8 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -24,7 +24,7 @@
 
         var setSong = function(song) {
             if (currentBuzzObject) {
-                currentBuzzObject.stop();
+                stopSong(song);
                 // currentSong.playing = null;
                 SongPlayer.currentSong.playing = null;
             }
@@ -46,6 +46,17 @@
         var playSong = function(song) {
             currentBuzzObject.play();
             song.playing = true;
+        };
+
+        /**
+        * @function stopSong
+        * @desc stops current Buzz object and sets playing attribute
+        * @param {Object} song
+        */
+
+        var stopSong = function(song) {
+            currentBuzzObject.stop();
+            song.playing = null;
         };
 
 
@@ -99,14 +110,33 @@
             currentSongIndex--;
 
             if (currentSongIndex < 0) {
-                currentBuzzObject.stop();
+                stopSong(song);
                 SongPlayer.currentSong.playing = song;
             } else {
                 var song = currentAlbum.songs[currentSongIndex];
                 setSong(song);
                 playSong(song);
             }
+        };
 
+
+        /**
+        * @function next
+        * @desc plays next song
+        * @param {Object} 
+        */
+
+        SongPlayer.next = function() {
+            var currentSongIndex = getSongIndex(SongPlayer.currentSong);
+            currentSongIndex++;
+
+            if (currentSongIndex > (currentAlbum.songs.length - 1)) {
+                stopSong(song);
+            } else {
+                var song = currentAlbum.songs[currentSongIndex];
+                setSong(song);
+                playSong(song);
+            }
         };
 
         return SongPlayer;
